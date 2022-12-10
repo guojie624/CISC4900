4e80d16f6c66257dc354f5fcad7213a0c2ad8e44
Kaihchen1230 <kaihang.c@gmail.com>
2022-12-10 17:11:17 -0500

    add authContext to handle authentication

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Route, Routes } from 'react-router-dom';
 import TodoList from './todoList/TodoList';
 import './App.css';
 import PrivateRoutes from './utils/PrivateRoute';
-// import { app, database } from './firebaseConfig';
-// import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
 	return (
 		<div>
-			<Routes>
-				<Route element={<PrivateRoutes />}>
-					<Route path='/calendar-page' element={<CalendarPage />}></Route>
-					<Route path='/todo-list' element={<TodoList />}></Route>
-				</Route>
-				<Route path='/' element={<Login />}></Route>
-			</Routes>
+			<AuthContextProvider>
+				<Routes>
+					<Route element={<PrivateRoutes />}>
+						<Route path='/calendar-page' element={<CalendarPage />}></Route>
+						<Route path='/todo-list' element={<TodoList />}></Route>
+					</Route>
+					<Route path='/' element={<Login />}></Route>
+				</Routes>
+			</AuthContextProvider>
 		</div>
 	);
 }
