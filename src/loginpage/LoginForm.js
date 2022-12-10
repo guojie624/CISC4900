08547769ea7c50d6e0ec7ffa817e5bda0852f107
Kaihchen1230 <kaihang.c@gmail.com>
2022-12-10 17:14:04 -0500

    remove unused codes

--- src/loginpage/LoginForm.js
+++ src/loginpage/LoginForm.js
@@ -10,18 +10,20 @@ import {
 } from 'firebase/auth';
 import { collection, addDoc, doc, setDoc } from 'firebase/firestore';
 import { database } from '../firebaseConfig';
+import { UserAuth } from '../context/AuthContext';
 
 const LoginForm = () => {
-	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const [buttonDisabled, SetButtonDisabled] = useState(false);
 	const [isSignUp, SetIsSignUp] = useState(false);
 	const navigate = useNavigate();
 	const usersCollectionRef = collection(database, 'users');
+	const { createUser, login } = UserAuth();
+
 	const setInputValue = (property, val) => {
 		val = val.trim();
-		if (property === 'username') {
-			setUsername(val);
+		if (property === 'email') {
+			setEmail(val);
 		}
 		if (property === 'password') {
 			setPassword(val);
@@ -31,29 +33,24 @@ const LoginForm = () => {
 		}
 	};
 	const resetForm = () => {
-		setUsername('');
+		setEmail('');
 		setPassword('');
-		SetButtonDisabled(false);
 	};
 
 	const submitForm = async () => {
-		const auth = getAuth();
+		// const auth = getAuth();
 		// console.log(auth);
 		// const currentUser = auth.currentUser;
 		// const currentUserUid = currentUser.uid;
 		if (isSignUp) {
 			try {
-				const responseUser = await createUserWithEmailAndPassword(
-					auth,
-					username,
-					password
-				);
+				const responseUser = await createUser(email, password);
 				console.log('this is responseUser - ', responseUser);
 
 				const newtUserUid = responseUser.user.uid;
 
 				await setDoc(doc(database, 'users', newtUserUid), {
-					email: username,
+					email: email,
 				});
 
 				// console.log(
@@ -75,7 +72,7 @@ const LoginForm = () => {
 			SetIsSignUp(false);
 			resetForm();
 		} else {
-			signInWithEmailAndPassword(auth, username, password)
+			login(email, password)
 				.then((response) => {
 					console.log(response.user);
 					// navigate('/calendar-page');
@@ -96,8 +93,8 @@ const LoginForm = () => {
 			<InputField
 				type='text'
 				placeholder='Username'
-				value={username ? username : ''}
-				onChange={(val) => setInputValue('username', val)}
+				value={email ? email : ''}
+				onChange={(val) => setInputValue('email', val)}
 			/>
 			<InputField
 				type='password'
@@ -108,7 +105,7 @@ const LoginForm = () => {
 
 			<SubmitButton
 				text={isSignUp ? 'Sign Up' : 'Log In'}
-				disabled={!username && !password}
+				disabled={!email && !password}
 				handleClick={submitForm}
 			/>
 
