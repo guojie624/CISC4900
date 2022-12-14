53278c5b88d7892c9c2aafb1f9d05dd7a2e4c262
guojie624 <guojie94520@gmail.com>
2022-12-14 16:57:56 -0500

    css update

--- src/loginPage/login.js
+++ src/loginPage/login.js
@@ -1,31 +1,28 @@
-import LoginForm from './LoginForm';
+import LoginForm from "./LoginForm";
 
-import './login.css';
-import React from 'react';
-import { observer } from 'mobx-react';
-import { AuthContextProvider } from '../context/AuthContext';
-import { Container } from '@mui/system';
+import "./login.css";
+import React from "react";
+import { observer } from "mobx-react";
+import { AuthContextProvider } from "../context/AuthContext";
+import { Container } from "@mui/system";
 
 const Login = () => {
-	return (
-		<div>
-			<h1 className='Title'>Wellcome to Calender Tracker</h1>
+  return (
+    <div>
+      <h1 className="Title">Welcome to Live Task Manager</h1>
 
       <AuthContextProvider>
         <div className="section">
           <div className="box1">
-          <LoginForm />
+            <LoginForm />
           </div>
           <div className="box2">
-            <h1>Introduction
-                 
-            </h1>
+            <h1>Introduction</h1>
             <p1>
-                Calender Tracker can record all your event online, 
-                you can login your account to view, 
-                add, or modified all your events anytime.
-                you can view all your event in the daily Todo List,
-                and Manager more in the Calender.
+              Live Task Manager can record all your event online, you can login
+              your account to view, add, or modified all your events anytime.
+              you can view all your event in the daily Todo List, and Manager
+              more in the Calender.
             </p1>
             <p2>Simply signup your account to manager your life here.</p2>
           </div>
